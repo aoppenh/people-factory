8cc703f87fc4951378a3101d2131450f858fbf65
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-05 14:24:36 -0400

    color changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const personName = document.querySelector('#personForm')
 const personAge = document.querySelector('#personForm')
+const personSchool = document.querySelector('#personForm')
 
 function handleSubmit(ev) {
     ev.preventDefault()
@@ -7,20 +8,23 @@ function handleSubmit(ev) {
     const f = ev.target
     const name = f.personName.value
     const age = f.personAge.value
+    const school = f.personSchool.value
 
     if (age < 0) {
         document.querySelector('p#welcome').innerHTML = 'You cannot enter a negative age, unless of course you are an unborn demon'
-        document.querySelector('p#welcome').style.color = 'crimson'
         document.querySelector('p#welcome').style.fontSize = 'xx-large'
-        document.body.style.backgroundColor = 'black'
+        document.body.style.backgroundColor = 'darkgrey'
+        document.querySelector('p#valid').style.color = 'crimson'
+        document.querySelector('p#welcome').style.color = 'crimson'
     } else {
-        document.querySelector('p#welcome').innerHTML = 'Welcome' + ' - ' + 'You are ' + age + ' years old.'
+        document.querySelector('p#welcome').innerHTML = 'Welcome' + ' - ' + 'You are ' + age + ' years old and you attend ' + school
         document.querySelector('p#welcome').style.color = 'black'
         document.querySelector('h1').innerHTML = 'People Factory - ' + name
-        document.querySelector('h1').style.color = 'darkgoldenrod'
+        document.querySelector('p#valid').style.color = 'black'
         document.body.style.backgroundColor = 'lightblue'
     }
 }
 
 personName.addEventListener('submit', handleSubmit)
 personAge.addEventListener('submit', handleSubmit)
+personSchool.addEventListener('submit', handleSubmit)
