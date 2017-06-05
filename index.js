08ac27b8fb20f0b1ac15f8fd055f7f24d29f7df2
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-05 13:53:28 -0400

    Finished assignment and bonus, added background color change

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
-    document.querySelector('h1').textContent = name
+    const age = f.personAge.value
+    if (age < 0) {
+        document.querySelector('p#welcome').innerHTML = 'You cannot enter a negative age, unless of course you are an unborn demon'
+        document.body.style.backgroundColor = 'crimson'
+    } else {
+        document.querySelector('p#welcome').innerHTML = 'Welcome' + '-' + name + ' ' + '\n You are ' + age + ' years old.'
+        document.body.style.backgroundColor = 'white'
+    }
 }
 
 personName.addEventListener('submit', handleSubmit)
