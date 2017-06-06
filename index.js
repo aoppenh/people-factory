e54699d613486ee97669624bcfc6d90e6a682160
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-06 10:49:39 -0400

    added details list

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const resetButton = document.querySelector('#resetForm')
 document.querySelector('p#welcome').innerHTML ='Welcome!'
 document.querySelector('p#welcome').style.fontSize = 'xx-large'
 
-
 function handleSubmit(ev) {
     ev.preventDefault()
 
@@ -15,15 +14,11 @@ function handleSubmit(ev) {
     const color = f.personColor.value
     const number = f.personNumber.value
 
-    document.querySelector('#details').querySelector('p').innerHTML = `\
-        <ul>
-            <li>Name: ${name}</li>
-            <li>Age: ${age}</li>
-            <li>School: ${school}</li>
-            <li>Color: ${color}</li>
-            <li>Number: ${number}</li>
-        </ul>
+    const colorDiv = `
+        <div style="background-color: ${color}; width: 100px; height: 50px;"></div>
     `
+
+    detailList(name, age, school, color, colorDiv, number)
 
     if (age < 0) {
         document.querySelector('p#welcome').innerHTML = 'You cannot enter a negative age!!!!!!!!'
@@ -36,7 +31,7 @@ function handleSubmit(ev) {
         document.querySelector('p#welcome').style.color = 'black'
         document.querySelector('h1').innerHTML = 'People Factory : ' + name + ' - Favorite Number : ' + number
         document.querySelector('p#valid').style.color = 'black'
-        document.body.style.backgroundColor = color
+        //document.body.style.backgroundColor = color
     }
 
     // const em = document.createElement('em')
@@ -57,6 +52,40 @@ function handleReset(ev) {
     document.querySelector('p#welcome').innerHTML ='Welcome!'
     document.querySelector('p#valid').style.color = 'black'
     document.querySelector('h1').innerHTML = 'A Factory of People'
+}
+
+function detailList(name, age, school, color, colorDiv, number) {
+    // document.querySelector('#details').querySelector('ul').innerHTML = `
+    //     <ul>
+    //         <p>Details:</p>
+    //         <li>Name: ${name}</li>
+    //         <li>Age: ${age}</li>
+    //         <li>School: ${school}</li>
+    //         <li>Color: ${color}</li>
+    //         <li>Color: ${colorDiv}</li>
+    //         <li>Number: ${number}</li>
+    //     </ul>
+    //  `
+
+    const details = document.querySelector('#details')
+    const text = document.createElement('text')
+
+    document.querySelector('#details').innerHTML = 'Details:'
+
+    text.innerHTML = `Details:`
+    details.appendChild(text)
+
+    text.innerHTML = `
+        <ul>
+            <li>Name: ${name}</li>
+            <li>Age: ${age}</li>
+            <li>School: ${school}</li>
+            <li>Color: ${color}</li>
+            <li>Color: ${colorDiv}</li>
+            <li>Number: ${number}</li>
+        </ul>
+    `
+    details.appendChild(text)
 }
 
 personInfo.addEventListener('submit', handleSubmit)
