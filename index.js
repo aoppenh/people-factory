642ec254b2c5f7fa6527eb62923d9138e7110f57
Andrew Oppenheimer <aoppenh@purdue.edu>
2017-06-07 09:52:38 -0400

    wrap in IIFE

--- index.js
+++ index.js
@@ -1,92 +1,96 @@
-const personInfo = document.querySelector('#personForm')
-const resetButton = document.querySelector('#resetForm')
+(function() {
+    const personInfo = document.querySelector('#personForm')
+    const resetButton = document.querySelector('#resetForm')
 
-document.querySelector('p#welcome').innerHTML ='Welcome!'
-document.querySelector('p#welcome').style.fontSize = 'xx-large'
+    document.querySelector('p#welcome').innerHTML = 'Welcome!'
+    document.querySelector('p#welcome').style.fontSize = 'xx-large'
 
-const details = document.querySelector('#details')
+    const details = document.querySelector('#details')
 
-function handleSubmit(ev) {
-    ev.preventDefault()
+    function handleSubmit(ev) {
+        ev.preventDefault()
 
-    const f = ev.target
-    const name = f.personName.value
-    const age = f.personAge.value
-    const school = f.personSchool.value
-    const color = f.personColor.value
-    const number = f.personNumber.value
+        const f = ev.target
+        const name = f.personName.value
+        const age = f.personAge.value
+        const school = f.personSchool.value
+        const color = f.personColor.value
+        const number = f.personNumber.value
 
-    // const colorDiv = `
-    //     <div style="background-color: ${color}; width: 100px; height: 50px;"></div>
-    // `
+        // const colorDiv = `
+        //     <div style="background-color: ${color}; width: 100px; height: 50px;"></div>
+        // `
 
-    const person = {
-        name: name,
-        age: age,
-        school: school,
-        number: number,
-        color: color,
-        colorDiv: renderColor(color).outerHTML,
+        const person = {
+            name: name,
+            age: age,
+            school: school,
+            number: number,
+            color: color,
+            colorDiv: renderColor(color).outerHTML,
+        }
+
+        details.appendChild(renderList(person))
+
+        if (age < 0) {
+            document.querySelector('p#welcome').innerHTML = 'You cannot enter a negative age!!!!!!!!'
+            document.querySelector('p#welcome').style.fontSize = 'xx-large'
+            document.querySelector('p#valid').style.color = 'crimson'
+            document.querySelector('p#welcome').style.color = 'crimson'
+        } else {
+            document.querySelector('p#welcome').innerHTML = 'Welcome' + ' : ' + 'You are ' + age + ' years old and you attend ' + school
+            document.querySelector('p#welcome').style.fontSize = 'xx-large'
+            document.querySelector('p#welcome').style.color = 'black'
+            document.querySelector('h1').innerHTML = name + ' - Age : ' + age
+            document.querySelector('p#valid').style.color = 'black'
+            //document.body.style.backgroundColor = color
+        }
+
+        // const em = document.createElement('em')
+        // em.textContent = name
+        // details.appendChild(em)
     }
 
-    details.appendChild(renderList(person))
+    function handleReset(ev) {
+        ev.preventDefault()
 
-    if (age < 0) {
-        document.querySelector('p#welcome').innerHTML = 'You cannot enter a negative age!!!!!!!!'
-        document.querySelector('p#welcome').style.fontSize = 'xx-large'
-        document.querySelector('p#valid').style.color = 'crimson'
-        document.querySelector('p#welcome').style.color = 'crimson'
-    } else {
-        document.querySelector('p#welcome').innerHTML = 'Welcome' + ' : ' + 'You are ' + age + ' years old and you attend ' + school
-        document.querySelector('p#welcome').style.fontSize = 'xx-large'
+        document.querySelector('#personForm').reset()
+        document.querySelector('#resetForm').reset()
+
+        //details.reset()
+
+        document.body.style.backgroundColor = 'white'
         document.querySelector('p#welcome').style.color = 'black'
-        document.querySelector('h1').innerHTML = name + ' - Age : ' + age
+        document.querySelector('p#welcome').innerHTML = 'Welcome!'
         document.querySelector('p#valid').style.color = 'black'
-        //document.body.style.backgroundColor = color
+        document.querySelector('h1').innerHTML = 'A Factory of People'
     }
 
-    // const em = document.createElement('em')
-    // em.textContent = name
-    // details.appendChild(em)
-}
+    function renderList(personData) {
+        const list = document.createElement('ul')
+        Object.keys(personData).map(function (fieldName) {
+            const li = renderListData(fieldName, personData[fieldName])
+            list.appendChild(li)
+        })
+        return list
+    }
 
-function handleReset(ev) {
-    ev.preventDefault()
+    function renderColor(color) {
+        const colorDiv = document.createElement('div')
+        colorDiv.style.backgroundColor = color
+        colorDiv.style.width = '100px'
+        colorDiv.style.height = '50px'
+        return colorDiv
+    }
 
-    document.querySelector('#personForm').reset()
-    document.querySelector('#resetForm').reset()
+    function renderListData(fieldName, value) {
+        const li = document.createElement('li')
+        li.innerHTML = `${fieldName}: ${value}`
+        return li
+    }
 
-    //details.reset()
+    personInfo.addEventListener('submit', handleSubmit)
+    resetButton.addEventListener('reset', handleReset)
+})()
 
-    document.body.style.backgroundColor = 'white'
-    document.querySelector('p#welcome').style.color = 'black'
-    document.querySelector('p#welcome').innerHTML ='Welcome!'
-    document.querySelector('p#valid').style.color = 'black'
-    document.querySelector('h1').innerHTML = 'A Factory of People'
-}
-
-function renderList(personData) {
-    const list = document.createElement('ul')
-    Object.keys(personData).map(function(fieldName) {
-        const li = renderListData(fieldName, personData[fieldName])
-        list.appendChild(li)
-    })
-    return list
-}
-
-function renderColor(color) {
-    const colorDiv = document.createElement('div')
-    colorDiv.style.backgroundColor = color
-    colorDiv.style.width = '100px'
-    colorDiv.style.height = '50px'
-    return colorDiv
-}
-
-function renderListData(fieldName, value) {
-    const li = document.createElement('li')
-    li.innerHTML = `${fieldName}: ${value}`
-    return li
-}
-
-personInfo.addEventListener('submit', handleSubmit)
-resetButton.addEventListener('reset', handleReset)
+//IIFE
